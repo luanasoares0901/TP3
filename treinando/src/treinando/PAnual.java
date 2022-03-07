126e2503c919d574347ae6957d01b28d6615e4e5
MillenaQueiroz <millena.queiroz08@gmail.com>
2022-03-07 15:49:04 -0300

    fix bug

--- treinando/src/treinando/PAnual.java
+++ treinando/src/treinando/PAnual.java
@@ -10,7 +10,7 @@ public class PAnual implements IPlanoEmpr{
     
     @Override
     public void cadastrarEmp() { 
-        // TODO Auto-generated method stub
+        // TODO Auto-generated method stub 
         
     }
 
