c20e69f86ec96a2299611a92ed50f4b6ca451978
MillenaQueiroz <millena.queiroz08@gmail.com>
2022-03-21 19:20:00 -0300

    add login view

---
+++ treinando/src/view/ViewLogin.java
@@ -0,0 +1,53 @@
+package view;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class ViewLogin {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("BiciGama");
+		frame.setSize(650, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		placeComponents(panel);
+
+		frame.setVisible(true);
+	}
+
+	private static void placeComponents(JPanel panel) {
+
+		panel.setLayout(null);
+
+		JLabel userLabel = new JLabel("Usuario");
+		userLabel.setBounds(200, 10, 80, 25);
+		panel.add(userLabel);
+
+		JTextField userText = new JTextField(20);
+		userText.setBounds(250, 10, 160, 25);
+		panel.add(userText);
+
+		JLabel passwordLabel = new JLabel("Senha");
+		passwordLabel.setBounds(200, 40, 80, 25);
+		panel.add(passwordLabel);
+
+		JPasswordField passwordText = new JPasswordField(20);
+		passwordText.setBounds(250, 40, 160, 25);
+		panel.add(passwordText);
+
+		JButton loginButton = new JButton("login");
+		loginButton.setBounds(282, 80, 80, 25);
+		panel.add(loginButton);
+		
+		JButton registerButton = new JButton("registrar-se");
+		registerButton.setBounds(270, 115, 105, 25);
+		panel.add(registerButton);
+	}
+}
+   
--- treinando/src/view/ViewMenu.java
+++ treinando/src/view/ViewMenu.java
@@ -1,5 +1,14 @@
 package view;
+import javax.swing.*;
 
 public class ViewMenu {
-
+	public static void main(String[] a) { 
+		JFrame janela = new JFrame("TESTE!"); 
+		JButton b =new JButton("click");
+		b.setBounds(130,100,100, 40);
+		janela.add(b);
+		janela.setSize(400,500);
+		janela.setLayout(null);
+		janela.setVisible(true);
+	} 
 }
